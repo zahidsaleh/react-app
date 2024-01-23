576922ea50d556ed98355dac7508da9cb437927a
Zahid Saleh <49088675+zahidsaleh@users.noreply.github.com>
2024-01-23 05:32:30 +0500

    some jsx changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
-function AboutPage() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
-export default AboutPage;
